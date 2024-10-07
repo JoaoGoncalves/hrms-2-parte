6a54080a78ef551eac43651ca6101ee376d7ef94
Joao Goncalves <me@papin.dev>
2024-10-07 15:55:54 +0100

    ajustes de comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,12 @@ export class AppComponent {
 /// first example of signals
 const count = signal(0);
 console.log(count());
+//! WritableSignal is a type of a Signal whose value can be changed (hence writable)
 
+//There are other signals, typed as simply Signal, which are immutable
+
+// signal function is written in a way that allows TypeScript to infer the type of the signal from the default value
+// there are cases when inferring the value is not really possible, For instance, we want to create a signal of an array of strings, but the default value is an empty Array, TypeScript has no way of guessing the overall type of this array
+//!we should provide the type manually:
 const names = signal<string[]>([]);
 console.log(names())
