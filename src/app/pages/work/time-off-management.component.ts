08192536ea99f5dd8e930f55f582ba4a224a4f15
Joao Goncalves <me@papin.dev>
2024-10-07 18:31:19 +0100

    ajustes ao serviço temporario, so com getRequests()

--- src/app/pages/work/time-off-management.component.ts
+++ src/app/pages/work/time-off-management.component.ts
@@ -1,3 +1,23 @@
+/*
+
+  - initial value of the requests signal an array of mock requests
+
+  - We will have a service, which will utilize the HttpClient and return an Observable of time-off requests. But if it is an Observable, how do I use it as a signal in my code?
+
+  - criar e serviço com o metodo getRequests() somente para já
+
+  //! all the methods where we changed its value via update will now throw errors
+  - because toSignal function returns a Signal, and not a WritableSignal, which is expected
+  - Observables can be async and have three states (working, error, and completed)
+  - this has to be reflected in some way in the signal derived from it
+  - Because of this, we cannot just go assigning signals derived from Observables to properties wherever we like
+
+  - resolveremos isso para já deixemos sem as funcionalidades restantes do serviço
+
+*/
+
+
+
 import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component, computed, effect, inject, signal } from '@angular/core';
 import { TimeOffRequest } from '../../infrastructure/types/time-off-request.type';
@@ -71,6 +91,26 @@ export class TimeOffManagementComponent {
   private readonly timeoffRequestService = inject(TimeOffRequestService);
   requests = toSignal(this.timeoffRequestService.getRequests(), {initialValue: []});
 
+  /* requests = signal<TimeOffRequest[]>([
+    {
+      id: 1,
+      employeeId: 1,
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+      type: 'Vacation',
+      status: 'Pending',
+    },
+    {
+      id: 2,
+      employeeId: 2,
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+      type: 'Sick Leave',
+      status: 'Approved',
+      comment: 'Feeling pretty sick today :(',
+    },
+  ]); */
+
   constructor() {
     effect(()=> {
       localStorage.setItem('selectedType', this.selectedType())
@@ -99,27 +139,27 @@ export class TimeOffManagementComponent {
   });
 
   approveRequest(request: TimeOffRequest) {
-    this.requests.update((requests) => {
+    /* this.requests.update((requests) => {
       const index = requests.findIndex((r) => r.id === request.id);
       return requests.map((item, i) =>
         i === index ? { ...item, status: 'Approved' } : item
       );
-    });
+    }); */
   }
 
   rejectRequest(request: TimeOffRequest) {
-    this.requests.update((requests) => {
+    /* this.requests.update((requests) => {
       const index = requests.findIndex((r) => r.id === request.id);
       return requests.map((item, i) =>
         i === index ? { ...item, status: 'Rejected' } : item
       );
-    });
+    }); */
   }
 
   deleteRequest(request: TimeOffRequest) {
-    this.requests.update((requests) =>
+    /* this.requests.update((requests) =>
       requests.filter((r) => r.id !== request.id)
-    );
+    ); */
   }
 }
 
