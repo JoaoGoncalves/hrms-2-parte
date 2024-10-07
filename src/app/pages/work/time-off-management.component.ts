9261d5a8579940d9c3d5cca90651d909adc697a5
Joao Goncalves <me@papin.dev>
2024-10-07 11:50:05 +0100

    computed signals on time-off-component

--- src/app/pages/work/time-off-management.component.ts
+++ src/app/pages/work/time-off-management.component.ts
@@ -1,13 +1,23 @@
 import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 import { TimeOffRequest } from '../../infrastructure/types/time-off-request.type';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-time-off-management',
   standalone: true,
-  imports: [NgIf, NgFor, DatePipe],
+  imports: [NgIf, NgFor, DatePipe, FormsModule],
   template: `
     <h2>Time Off Management</h2>
+    <h3>Resolved {{ resolvedRequests().length }} / {{  filteredRequests().length }} Unresolved </h3>
+    <select [ngModel]="selectedType()" (ngModelChange)="selectedType.set($any($event))">
+    <option value="">All</option>
+    <option value="Vacation">Vacation</option>
+    <option value="Sick Leave">Sick Leave</option>
+    <option value="Maternity Leave">Maternity Leave</option>
+    <option value="Paternity Leave">Paternity Leave</option>
+    <option value="Other">Other</option>
+</select>
     <table>
       <thead>
         <tr>
@@ -21,7 +31,7 @@ import { TimeOffRequest } from '../../infrastructure/types/time-off-request.type
         </tr>
       </thead>
       <tbody>
-        <tr *ngFor="let request of requests()">
+        <tr *ngFor="let request of filteredRequests()">
           <td>{{ request.employeeId }}</td>
           <td>{{ request.startDate | date }}</td>
           <td>{{ request.endDate | date }}</td>
@@ -29,10 +39,18 @@ import { TimeOffRequest } from '../../infrastructure/types/time-off-request.type
           <td>{{ request.status }}</td>
           <td>{{ request.comment }}</td>
           <td>
-            <button *ngIf="request.status === 'Pending'"
-            (click)="approveRequest(request)">Approve</button>
-            <button *ngIf="request.status === 'Pending'"
-            (click)="rejectRequest(request)">Reject</button>
+            <button
+              *ngIf="request.status === 'Pending'"
+              (click)="approveRequest(request)"
+            >
+              Approve
+            </button>
+            <button
+              *ngIf="request.status === 'Pending'"
+              (click)="rejectRequest(request)"
+            >
+              Reject
+            </button>
             <button (click)="deleteRequest(request)">Delete</button>
           </td>
         </tr>
@@ -61,6 +79,23 @@ export class TimeOffManagementComponent {
       comment: 'Feeling pretty sick today :(',
     },
   ]);
+
+  selectedType = signal<
+    'Vacation' | 'Sick Leave' | 'Maternity Leave' | 'Paternity Leave' | 'Other' | ''
+  >('');
+
+
+  resolvedRequests = computed(() =>
+    //this.requests().filter(r => r.status !== 'Pending')
+    this.filteredRequests().filter(r => r.status !== 'Pending')
+  );
+
+  filteredRequests = computed(() => {
+    const type = this.selectedType();
+    return this.requests().filter(r => (type ? r.type === type : true));
+
+  });
+
 
   approveRequest(request: TimeOffRequest) {
     this.requests.update((requests) => {
