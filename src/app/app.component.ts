447116cf791bdb575f91cd0e1d0ecc1feb349001
Joao Goncalves <me@papin.dev>
2024-10-07 16:25:59 +0100

    ajustes de comentarios no codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,11 +21,14 @@ export class AppComponent {
 /// first example of signals
 const count = signal(0);
 console.log(count());
+//TypeScript inferred the type of this signal, so in the case of count, the set method will only accept numbers
 count.set(1);
 console.log(count());
+//We can also use other signals as values when setting a value of some signal, for instance
 count.set(count() + 1);
 console.log(count());
 
+//The update method of a signal will accept a callback
 const increment = () => count.update(value => value + 1);
 increment();
 console.log(count());
@@ -35,9 +38,16 @@ console.log(count());
 console.log('--------------------');
 
 const names = signal<string[]>([]);
+//update method becomes very useful when working with arrays or objects
 const addName = (name: string) => names.update( value => [...value, name])
 console.log(names())
 addName('Joao');
 console.log(names())
 addName('José');
 console.log(names())
+//!The callback for the update method can be any function, and we can perform a variety of operations and logical conditions here, provided it matches the types
+
+//? guidelines as to using the update method
+//? use pure functions as callbacks, always return the same value when called, and do not have side-effects
+
+
