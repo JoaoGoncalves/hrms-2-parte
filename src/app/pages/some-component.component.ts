45c91266b5e3e3a36805f6e70d1947115c876981
Joao Goncalves <me@papin.dev>
2024-10-13 16:11:57 +0100

    7.2.2 how effects work

--- src/app/pages/some-component.component.ts
+++ src/app/pages/some-component.component.ts
@@ -11,62 +11,25 @@ import { interval, map } from 'rxjs';
   template: `
 
     <article>
-    <form [formGroup]="nameForm" (ngSubmit)="onSubmit()">
-      <label for="firstName">First Name</label>
-      <input id="firstName" formControlName="firstName" type="text">
+      <button (click)="update()">update</button>
 
-      <label for="lastName">Last Name</label>
-      <input id="lastName" formControlName="lastName" type="text">
-
-      <button type="submit">Submit</button>
-    </form>
-
-    <div *ngIf="submitted">
-      <p>First Name: {{ nameForm.value.firstName }}</p>
-      <p>Last Name: {{ nameForm.value.lastName }}</p>
-    </div>
     </article>
   `,
 })
 export class SomeComponent {
-  fb = inject(FormBuilder);
-  nameForm: FormGroup;
-  submitted = false;
 
-  user = signal({
-    id: 1,
-    firstName: 'Jon',
-    lastName: 'Snow',
-    age: 20,
-  });
+  counter = signal(0);
 
-  dateTime = toSignal(interval(2000).pipe(map(() => new Date())), {initialValue: new Date()})
-
-  constructor() {
-    this.nameForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-    });
-
-
-    effect(()=> {
-      const dateTime = this.dateTime;
-      untracked(()=>{
-        const formValues = this.nameForm.value;
-        localStorage.setItem('formValues', JSON.stringify(formValues));
-        alert(`Your PRgress has been saved at ${dateTime}`)
-      })
+  constructor(){
+    effect(() => {
+      console.log(`Value is: ${this.counter()}`);
     })
   }
 
-  onSubmit() {
-    this.submitted = true;
-    this.user.update(value => ({
-      ...value,
-      firstName: this.nameForm.value,
-      lastName: this.nameForm.value,
-    }));
+  update(){
+    this.counter.set(7);
+    this.counter.set(11)
+    this.counter.set(20)
   }
-
 
 }
