f7ef1a7fec1d07c536fbb0e5627a976b4beba2b9
Joao Goncalves <me@papin.dev>
2024-10-16 23:55:23 +0100

    output signal

--- src/app/shared/componentes/file-upload.component.ts
+++ src/app/shared/componentes/file-upload.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-file-upload',
@@ -9,28 +9,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     <section>
       <label for="upload">{{label}}</label>
       <input type="file" id="upload" (change)="onFileSeleted($event)">
-      <div *ngIf="errorMessage">
-        {{errorMessage}}
-        <p>Only the following files are accepted</p>
-        <ul>
-          <li *ngFor="let type of accept">{{type}}</li>
-        </ul>
-      </div>
+      @if (errorMessage) {
+        <div>
+          {{errorMessage}}
+          <p>Only the following files are accepted</p>
+          <ul>
+            @for (type of accept() ; track type) {
+              <li>{{type}}</li>
+            }
+          </ul>
+        </div>
+      }
     </section>
   `,
   styles: ``
 })
 export class FileUploadComponent {
 
-  @Input({required: true}) label!: string;
+  /* @Input({required: true}) label!: string;
   @Input({transform: (value: string) => value.split(',')}) accept: string[] = [];
-  @Output() selected = new EventEmitter<FileList>();
+  @Output() selected = new EventEmitter<FileList>(); */
+
+  //! transformar os inputs e outputs em signals, com as respectivas fuções
+  label = input.required<string>();
+  accept = input([], {transform: (value: string) => value.split(',')});
+  selected = output<FileList>()
 
   errorMessage = '';
 
   onFileSeleted(event: any) {
     const files: FileList = event.target.files;
-    this.errorMessage = Array.from(files).every( f => this.accept.includes(f.type)) ? '' : 'Invalid File Type';
+    this.errorMessage = Array.from(files).every( f => this.accept().includes(f.type)) ? '' : 'Invalid File Type';
 
     if(this.errorMessage === ''){
       this.selected.emit(files);
